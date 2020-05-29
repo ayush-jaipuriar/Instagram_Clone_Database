22e69e885dda4dee575ccfbc40edf9da67cb7f46
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-05-29 23:26:55 +0530

    Challenge 7: Find the bots, they like every pic

---
+++ ig_clone_challege7.sql
@@ -0,0 +1,11 @@
+/*
+Find the bots. They like every pic
+*/
+SELECT username,
+    Count(*) AS num_likes
+FROM users
+    INNER JOIN likes
+    ON users.id = likes.user_id
+GROUP  BY likes.user_id
+HAVING num_likes = (SELECT Count(*)
+FROM photos); 
